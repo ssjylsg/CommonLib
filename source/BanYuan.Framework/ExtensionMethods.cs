47fcd5af8d0a719f162ddf90a5a392c5a648c3c6
ssjylsg <ssjylsg@126.com>
2012-08-28 16:05:14 +0800

    update

---
+++ source/BanYuan.Framework/ExtensionMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Castle.Windsor;
+
+namespace BanYuan.Framework
+{
+    public static class ExtensionMethods
+    {
+        public static void RegisterRepositories(this IWindsorContainer container, params  Assembly[] assembly)
+        {
+
+        }
+        public static void RegisterServices(this IWindsorContainer container, Assembly[] assembly, Type interceptor)
+        {
+
+        }
+        public static void RegisterRepositories(this IWindsorContainer container, Func<Type,bool> func, Assembly[] assembly)
+        {
+
+        }
+        public static void RegisterComponent(this IWindsorContainer container, Assembly[] assembly, Type interceptor)
+        {
+
+        }
+        public static void RegisterFromInterface(this IWindsorContainer container, Func<Type,bool> func, Assembly[] assembly)
+        {
+
+        }
+
+    }
+}
